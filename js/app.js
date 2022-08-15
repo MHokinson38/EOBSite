75fb71ddf67be138789cac7c2f8a060ae6eb217d
MHokinson38 <matthew@hokinson.com>
2022-08-15 18:05:08 -0500

    Initial site (copied)

---
+++ js/app.js
@@ -0,0 +1,10 @@
+var app = angular.module("eob_site",["ngRoute"]);
+app.config(function($routeProvider) {
+    $routeProvider.when("/home",       { templateUrl:"partials/home.html", controller:"homeController"})
+                  .when("/tours",      { templateUrl:"partials/tours.html", controller:"toursController"})
+                  .when("/edays",      { templateUrl:"partials/edays.html", controller:"edaysController"})
+                  .when("/advocates/", { templateUrl:"partials/advocates.html", controller:"advocatesController"})
+                  .when("/shadow/",    { templateUrl:"partials/shadow.html", controller:"shadowController"})
+                  .otherwise(          {redirectTo:"/home"})
+    }
+);
